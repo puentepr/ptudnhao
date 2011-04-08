322a3492bc8e7a0835628949b6c96e8e884175ee
nhieunguyenkhtn <nhieunguyenkhtn@gmail.com>
2011-04-08 13:27:39 +0000

    commit thieu may file, gio them vao phan truoc ;))

---
+++ SourceCode/BIZ/BIZ/GUI/Consumers/Register.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/GUI/Shared/MasterPage.Master" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="BIZ.GUI.Consumers.Register" %>
+<%@ Register src="../UserControls/Catalogs.ascx" tagname="Catalogs" tagprefix="uc1" %>
+<%@ Register src="../UserControls/Cart.ascx" tagname="Cart" tagprefix="uc3" %>
+<%@ Register src="../UserControls/NewProduce.ascx" tagname="NewProduce" tagprefix="uc4" %>
+<%@ Register src="../UserControls/AdvertisementLeft.ascx" tagname="AdvertisementLeft" tagprefix="uc5" %>
+<%@ Register src="../UserControls/Payment.ascx" tagname="Payment" tagprefix="uc8" %>
+<%@ Register src="../UserControls/ConsumerRegister.ascx" tagname="ConsumerRegister" tagprefix="uc2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="title" runat="server">
+Đăng ký tài khoản
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="include" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+<!-- begin first-secondary -->
+    <div class="first-secondary">
+        <!-- begin catalog-box -->
+         
+         <uc1:Catalogs ID="Catalogs1" runat="server" />
+        <!-- end catalog-box -->
+        <uc4:NewProduce ID="NewProduce1" runat="server" />
+        <!-- begin advleft-box -->
+        
+        <uc5:AdvertisementLeft ID="AdvertisementLeft1" runat="server" />
+        <!-- end advleft-box -->
+        
+    </div>
+    <!-- end first-secondary -->
+    
+    <!-- begin primary -->
+    <div class="primary">
+        
+        <uc2:ConsumerRegister ID="ConsumerRegister1" runat="server" />
+        
+    </div>
+    <!-- end primary -->
+    
+    <!-- begin second-secondary -->
+    <div class="second-secondary">
+        <uc3:Cart ID="Cart1" runat="server" />
+        <uc8:Payment ID="Payment1" runat="server" />
+    </div>
+    <!-- begin second-secondary -->
+</asp:Content>
---
+++ SourceCode/BIZ/BIZ/GUI/UserControls/ConsumerRegister.ascx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BUS;
+
+namespace BIZ.GUI.UserControls
+{
+    public partial class ConsumerRegister : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void showMessage(string content)
+        {
+            Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "", "<script language='javascript'>alert('" + content + "');</script>");   
+        }
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+           /* showMessage("cureenExFilPath" + Request.CurrentExecutionFilePath.ToString());
+            //GUI/Register.aspx
+           // showMessage("Filepa " + Request.FilePath);
+           // showMessage("path " + Request.Path);
+            //showMessage("pathinf " + Request.PathInfo); hong co
+           // showMessage("physicapp " + Request.PhysicalApplicationPath);
+           // showMessage("phsicpath " + Request.PhysicalPath);//dia chi day du
+            //showMessage("rawurl " + Request.RawUrl);*/
+           // showMessage("absolutepath " + Request.Url.AbsolutePath);//BUI/Consumer
+           // showMessage("absoluteuri " + Request.Url.AbsoluteUri);// http://localhost:1155/GUI/Consumers/Register.aspx
+           // showMessage("hostname " + Request.UserHostName);//127.1.1
+           // showMessage(Request.UserHostAddress);
+           captcha.ValidateCaptcha(txtCaptcha.Text);
+
+            if (captcha.UserValidated)
+            {
+                string body = "<h4>Hi,username</h4><b>Doanh nghiệp bạn đã đăng ký thành công tại beatifulHouseGroup</b><br/>";
+                body += "<b style='color:blue'>Hãy click link bên dưới để hoàn tất việc đăng ký </b><br/>";
+                string fullpath= Request.Url.AbsoluteUri;
+                int index=fullpath.LastIndexOf("/");
+                fullpath= fullpath.Remove(index);
+                body+="<b>"+fullpath+"/Active.aspx"+"</b>";//chỗ này khi code sẽ kèm tham số là username+ băm gì đó
+                string subject="Kich hoat tai khoan";
+                Email.sendMail("nhieunguyenkhtn@gmail.com", body,subject);
+                bool test = Email.sendMail("forgetforgive1986@gmail.com", body,subject);
+                lbCaptcha.Text = test.ToString();
+                lbCaptcha.Visible = true;
+
+            }
+            else
+            {
+                lbCaptcha.Text = "Mã an toàn không đúng !";
+                txtCaptcha.Text = "true";
+                lbCaptcha.Visible = true;
+                return;
+            }
+            
+        }
+    }
+}
