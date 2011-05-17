233b5a7177f68870eaf71e1330c40b2f19d0180f
anhngoc.tg@gmail.com <anhngoc.tg@gmail.com@eb441d56-a3ec-77b4-58a4-bdad94bc4ce2>
2011-05-17 12:55:01 +0000

    wuc XemDanhSachTaiKhoan

---
+++ trunk/SourceCode/BIZ/BIZ/GUI/UserControls/XemDanhSachTaiKhoan.ascx
@@ -0,0 +1,58 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="XemDanhSachTaiKhoan.ascx.cs" Inherits="BIZ.GUI.UserControls.XemDanhSachTaiKhoan" %>
+<style type="text/css">
+    .style1
+    {
+        width: 100%;
+    }
+    .style2
+    {
+        text-align: left;
+        font-weight: bold;
+    }
+    .style3
+    {
+        text-align: left;
+        font-weight: bold;
+        color: #3333FF;
+    }
+</style>
+<table cellpadding="0" cellspacing="0" class="style1">
+    <tr>
+        <td class="style3">
+            XEM DANH SÁCH TÀI KHOẢN</td>
+    </tr>
+    <tr>
+        <td class="style2">
+            &nbsp;</td>
+    </tr>
+    <tr>
+        <td style="text-align: left">
+            <asp:GridView ID="gvDSTaiKhoan" runat="server" AutoGenerateColumns="False" 
+                CellPadding="4" ForeColor="#333333" GridLines="None" 
+                style="text-align: left" onrowcreated="gvDSTaiKhoan_RowCreated" 
+                AllowPaging="True">
+                <RowStyle BackColor="#EFF3FB" />
+                <Columns>
+                    <asp:BoundField DataField="UserName" HeaderText="UserName" />
+                    <asp:BoundField DataField="Email" HeaderText="Email" />
+                    <asp:TemplateField HeaderText="Vai trò">
+                        <ItemTemplate>
+                            <asp:Label ID="lbVaiTro" runat="server" Text="Label"></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Trạng thái">
+                        <ItemTemplate>
+                            <asp:Label ID="lbTrangThai" runat="server" Text="Label"></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+                <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <EditRowStyle BackColor="#2461BF" />
+                <AlternatingRowStyle BackColor="White" />
+            </asp:GridView>
+        </td>
+    </tr>
+</table>
---
+++ trunk/SourceCode/BIZ/BIZ/GUI/UserControls/XemDanhSachTaiKhoan.ascx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+//using BIZ.DAO;
+using BIZ.BUS;
+using BIZ.DTO;
+namespace BIZ.GUI.UserControls
+{
+    public partial class XemDanhSachTaiKhoan : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                gvDSTaiKhoan.DataSource = UserBUS.LayDanhSachUser();
+                gvDSTaiKhoan.DataBind();
+            }
+        }
+
+        protected void gvDSTaiKhoan_RowCreated(object sender, GridViewRowEventArgs e)
+        {
+            //e.Row.RowIndex != -1 : không chỉnh bất kỳ dòng nào
+            if (e.Row.RowIndex != -1 && e.Row.DataItem != null)
+            {
+                GridViewRow newRow = e.Row;
+                USERS_DTO user = (USERS_DTO)newRow.DataItem;
+
+                #region hiển thị text cho vai trò
+                Label lb1 = (Label)newRow.Cells[2].FindControl("lbVaiTro");
+                int vaitro = user.MaLoaiUser;
+                switch (vaitro)
+                {
+                    case 1:
+                        lb1.Text = "Admin";
+                        break;
+                    case 2:
+                        lb1.Text = "Manager";
+                        break;
+
+                    case 3:
+                        lb1.Text = "Consumer";
+                        break;
+                    default:
+                        break;
+                }
+                #endregion
+                                
+                #region hiển thị text cho trạng thái
+                Label lb2 = (Label)newRow.Cells[3].FindControl("lbTrangThai");
+                int trangthai = user.TrangThai;
+                switch (trangthai)
+                {
+                    case 0:
+                        lb2.Text = "chưa active";
+                        break;
+                    case 1:
+                        lb2.Text = "đã active";
+                        break;
+                    default:
+                        break;
+                }
+                #endregion             
+                
+            }
+        }
+
+        protected void gvDSTaiKhoan_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    
+       
+    }
+}
