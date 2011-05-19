abe87f35e1ef78bb298a30b64f18d53c7ea78c34
anhngoc.tg@gmail.com <anhngoc.tg@gmail.com@eb441d56-a3ec-77b4-58a4-bdad94bc4ce2>
2011-05-19 16:07:22 +0000

    commit ServiceBankBUS

---
+++ trunk/SourceCode/BIZ/BIZ/BUS/ServiceBankBUS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BIZ.DAO;
+using BIZ.DTO;
+
+namespace BIZ.BUS
+{
+    public class ServiceBankBUS
+    {
+        #region Thêm 1 ServiceBank
+        public static int InsertServiceBank(SERVICE_BANK_DTO bank)
+        {
+            try
+            {
+                return ServiceBankDAO.InsertServiceBank(bank);
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region Lấy danh sách ServiceBank đang sử dung
+        public static List<SERVICE_BANK_DTO> LayDanhSachServiceBank(int trangthai)
+        {
+            try
+            {
+                return ServiceBankDAO.LayDanhSachServiceBankTheoTrangThai(trangthai);
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region Xóa ServiceBank
+        #endregion
+    }
+}
